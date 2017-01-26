2bc40b7a66d4f89645997fd9a3a53b60da45ea36
Henry Zhang <henry.alakazhang@gmail.com>
2017-01-26 23:02:37 +1100

    added ungrouped analysis for offense

--- src/offense.js
+++ src/offense.js
@@ -3,7 +3,7 @@
  */
 
 import React, { Component } from 'react';
-import { Alert, Checkbox, Table, Button, Collapse, Well, Col, FormControl, Row, Form, Tabs, Tab} from 'react-bootstrap';
+import { Alert, Checkbox, Table, Button, Collapse, Well, Col, FormControl, Row, Form, Tabs, Tab, OverlayTrigger, Tooltip} from 'react-bootstrap';
 import { HorizontalInputComponent, TeamTableHead } from './General.js';
 
 import { types, getTypeCombo, getEffectiveness } from './data/types.js';
@@ -14,6 +14,25 @@ import { calculateDamage} from './data/datautil.js';
 const OFFENSE_CATEGORIES = {
   adv: ["1HKO", "2HKO", "3HKO", "4HKO", "5HKO"],
   base: ["4x", "2x", "1x", "0.5x", "0.25x", "0x"]
+}
+
+const MATCHUP_MESSAGES = {
+  adv : {
+    danger  : "Your team is very vulnerable to this type.",
+    warning : "Your team has a few Pokemon that are weak, but also a few that are strong to this type.",
+    weakish : "You have some squishy Pokemon but nobody to tank this type.",
+    nobreak : "None of your Pokemon can take many hits of this type.",
+    neutral : "Some of your Pokemon can tank this type.",
+    strong  : "Your team can tank this type comfortably."
+  },
+  base : {
+    danger  : "Your team cannot hit this type combination well at all!",
+    warning : "Some of your team can hit this type well, but others can't.",
+    weakish : "Your team is somewhat weak to this type.",
+    nobreak : "Your team has no Pokemon good against this type.",
+    neutral : "Your team is mostly neutral to this type.",
+    strong  : "Your team can deal with this type quite well."
+  }
 }
 
 /*
@@ -44,14 +63,16 @@ class Offense extends Component {
   buildOffenseMatrix() {
     const categories = (this.props.adv) ? OFFENSE_CATEGORIES.adv : OFFENSE_CATEGORIES.base;
     var newMatrix = { byCategory : [], byMatchup: []};
+    newMatrix.matchups = [];
     for (var i = 0; i < 6; i++) {
-      newMatrix.matchups = [];
       newMatrix.byCategory[i] = null;
       newMatrix.byMatchup[i] = null;
       // check if this mon has moves
       if (this.props.team[i].moves.length === 0)
         continue;
 
+      // replace here since we don't want matchups to overfill
+      newMatrix.matchups = [];
       newMatrix.byMatchup[i] = {};
       newMatrix.byCategory[i] = {};
       for (var cat = 0; cat < categories.length; cat++)
@@ -195,7 +216,7 @@ class Offense extends Component {
                 teamsize={teamsize}
                 condition={(poke) => poke.species != null && poke.moves.length > 0}
               />
-              <OffenseTableGrouped team={this.props.team} adv={this.props.adv} categories={categories} matrix={offenseMatrix} />
+              <OffenseTableGrouped team={this.props.team} adv={this.props.adv} categories={categories} matrix={offenseMatrix.byCategory} />
             </Table>
           </Tab>
           <Tab eventKey={2} title="Full">
@@ -205,7 +226,7 @@ class Offense extends Component {
                 teamsize={teamsize}
                 condition={(poke) => poke.species != null && poke.moves.length > 0}
               />
-              <OffenseTableFull team={this.props.team} adv={this.props.adv} matchups={offenseMatrix.matchups} matrix={offenseMatrix} />
+              <OffenseTableFull team={this.props.team} adv={this.props.adv} matchups={offenseMatrix.matchups} matrix={offenseMatrix.byMatchup} />
             </Table>
           </Tab>
         </Tabs>
@@ -220,7 +241,7 @@ function OffenseTableGrouped(props) {
       {props.categories.map((c) =>
         <tr key={c}>
           <th className="text-center" style={{"verticalAlign":"middle", "fontSize":"20px"}}>{c}</th>
-          {props.matrix.byCategory.map((poke, index) =>
+          {props.matrix.map((poke, index) =>
             (props.team[index].species != null && props.team[index].moves.length > 0)
             ? (props.adv)
               ? <OffenseMatchupAdvanced poke={poke} mult={c} key={index} />
@@ -231,23 +252,6 @@ function OffenseTableGrouped(props) {
       )}
     </tbody>
   );
-}
-
-function OffenseTableFull(props) {
-  return (
-    <tbody>
-      {props.matchups.map((c) =>
-        <tr key={c}>
-          <th className="text-center" style={{"verticalAlign":"middle", "fontSize":"20px"}}>{c}</th>
-          {props.matrix.byMatchup.map((poke, index) =>
-            (props.team[index].species != null && props.team[index].moves.length > 0)
-            ? null
-            : null
-          )}
-        </tr>
-      )}
-    </tbody>
-  )
 }
 
 class OffenseMatchup extends Component {
@@ -323,6 +327,134 @@ class OffenseMatchupAdvanced extends Component {
       </th>
     )
   }
+}
+
+function OffenseTableFull(props) {
+  return (
+    <tbody>
+      {props.matchups.map((c) =>
+        <tr key={c}>
+          <OffenseEntryOverall matchup={c} adv={props.adv} matrix={props.matrix}/>
+          {props.matrix.map((val, index) =>
+            (val != null) &&
+              <OffenseEntryIndiv
+                key={c+index}
+                value={val[c]}
+                adv={props.adv}
+              />
+          )}
+        </tr>
+      )}
+    </tbody>
+  )
+}
+
+function OffenseEntryIndiv(props) {
+  if (props.value === null) return null;
+  var color = "";
+  var val = props.value;
+  if (props.adv) {
+    val = Math.round(val+0.5);
+    if (val <= 0) {
+      color = "success"
+    } else if (val <= 1) {
+      color = "danger"
+    } else if (val <= 2) {
+      color = "warning"
+    } else if (val <= 3) {
+      color = "info"
+    } else {
+      color = "success"
+    }
+  } else {
+    if (val > 1) {
+      color = "success"
+    } else if (val === 1) {
+      color = "info"
+    } if (val < 1) {
+      color = "warning"
+    } if (val < 0.5) {
+      color = "danger"
+    }
+  }
+  val = (props.adv) ? Math.round(100/props.value) + "% (" + val + "HKO)" : val;
+  return <td className={color + " text-center"}>{val}</td>
+}
+
+function OffenseEntryOverall(props) {
+  var total = [0,0,0];
+  for (var i = 0; i < props.matrix.length; i++) {
+    if (props.matrix[i] === null) continue;
+    var mul = props.matrix[i][props.matchup];
+    if (props.adv) {
+      if (mul <= 0) {
+        // immunity or absorption
+        total[2] += 1.5;
+      } else if (mul < 1) {
+        // one hit KO
+        total[0] += 1;
+      } else if (mul < 1.9) {
+        // guaranteed two hit KO through lefties
+        total[0] += 1;
+      } else if (mul < 3) {
+        // three hit KO
+        total[1] += 1;
+      } else {
+        // four hit or better
+        total[2] += 1;
+      }
+    } else {
+      if (mul === 1) {
+        total[1] += 1;
+      } else if (mul < 1) {
+        total[0] += 1;
+        if (mul < 0.5) total[0] += 0.5; // 4x resists are walled HARD
+      } else if (mul > 1) {
+        total[2] += 1;
+        if (mul > 2) total[2] += 0.5; // 4x weaks are
+      }
+    }
+  }
+
+  var messages = (props.adv) ? MATCHUP_MESSAGES.adv : MATCHUP_MESSAGES.base;
+  var color, tip;
+  // walls a lot
+  if (total[0] > 2.5) {
+    // but can be broken
+    if (total[2] > 1.5) {
+      color = "warning";
+      tip = messages.warning;
+    // straight up walled
+    } else {
+      color = "danger";
+      tip = messages.danger;
+    }
+  // can break easily
+  } else if (total[2] > 2) {
+    color = "success";
+    tip = messages.strong;
+  // can't hit super effectively
+  } else if (total[2] === 0) {
+    // but sometimes walled
+    if (total[0] > 1) {
+      color = "danger";
+      tip = messages.weakish;
+    // but also never walled
+    } else {
+      color = "warning"
+      tip = messages.nobreak;
+    }
+  // everything else
+  } else {
+    color = "info"
+    tip = messages.neutral;
+  }
+
+  return (
+    <OverlayTrigger placement="top" overlay={<Tooltip id={props.value+"-help"}>{tip}</Tooltip>}>
+      <td className={color + " text-center"}>{props.matchup}</td>
+    </OverlayTrigger>
+  )
 }
 
 /*
